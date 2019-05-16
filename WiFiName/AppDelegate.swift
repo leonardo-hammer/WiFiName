f2180a26c093ca51dea8f3a0bab4b8793f847d51
tuesleep <tuesleep@gmail.com>
2019-05-16 11:25:26 +0800

    init

--- WiFiName/AppDelegate.swift
+++ WiFiName/AppDelegate.swift
@@ -7,21 +7,51 @@
 //
 
 import Cocoa
+import CoreWLAN
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
+    let wifiClient = CWWiFiClient.shared()
 
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength);
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        let interface = wifiClient.interface();
+
+        let button = statusItem.button
+        button?.font = NSFont.userFont(ofSize: 9)
+        button?.alignment = NSTextAlignment.center
+
+        if let ssid = interface?.ssid() {
+            button?.title = "Wi-Fi:\n \(ssid)"
+        } else {
+            button?.title = "No connect"
+        }
+
+        wifiClient.delegate = self
+        try! wifiClient.startMonitoringEvent(with: .ssidDidChange)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
 }
 
+extension AppDelegate: CWEventDelegate {
+    func ssidDidChangeForWiFiInterface(withName interfaceName: String) {
+        let interface = wifiClient.interface();
+
+        let button = statusItem.button
+
+        DispatchQueue.main.async {
+            if let ssid = interface?.ssid() {
+                button?.title = "Wi-Fi:\n \(ssid)"
+            } else {
+                button?.title = "No connect"
+            }
+        }
+    }
+}
