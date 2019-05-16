3a0d5adbcc873027a6731a42f067a5fbc9f935e5
tuesleep <tuesleep@gmail.com>
2019-05-16 09:26:48 +0800

    Initial Commit

---
+++ WiFiName/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  WiFiName
+//
+//  Created by Leonardo Hammer on 2019/5/16.
+//  Copyright © 2019 tuesleep. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
